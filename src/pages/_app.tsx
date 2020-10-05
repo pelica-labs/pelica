358a7568695d09284b5e4daa780cb4898e97566b
Bastien Formery <bastien.formery@gmail.com>
2020-10-05 18:19:18 +0200

    Remove tooltips

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,7 @@ import "~/styles/index.css";
 
 import { AppProps } from "next/app";
 import React from "react";
-import ReactTooltip from "react-tooltip";
-
-import { theme } from "~/lib/tailwind";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  return (
-    <>
-      <Component {...pageProps} />
-      <ReactTooltip border borderColor={theme.colors.green[500]} delayShow={300} effect="solid" />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
