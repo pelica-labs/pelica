b68143d8286c9623672cc90eebcfb21d95407557
Bastien Formery <bastien.formery@gmail.com>
2020-10-09 11:24:49 +0200

    Head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "~/styles/index.css";
 
 import { AppProps } from "next/app";
+import Head from "next/head";
 import React from "react";
 
 import { setupModals } from "~/lib/modals";
@@ -8,5 +9,15 @@ import { setupModals } from "~/lib/modals";
 setupModals();
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Pelica</title>
+        <link href="/images/logo.png" rel="shortcut icon" type="image/png" />
+        <link href="/images/logo.png" rel="apple-touch-icon" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
