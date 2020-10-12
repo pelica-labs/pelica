6fc9cb4171eec378d23fb65405e677fb11b65760
Bastien Formery <bastien.formery@gmail.com>
2020-10-12 14:33:45 +0200

    Rename

--- src/pages/api/upload-map.ts
+++ src/pages/api/upload-map.ts
@@ -7,8 +7,8 @@ import { NextApiHandler, NextApiRequest } from "next";
 import uniqid from "uniqid";
 
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_ACCESS_SECRET,
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_SECRET,
   region: process.env.AWS_S3_REGION,
 });
 
