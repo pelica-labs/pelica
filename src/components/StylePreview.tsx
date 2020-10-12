f2fa9c34ee65708a012b4e108dd3acbe242fad70
Bastien Formery <bastien.formery@gmail.com>
2020-10-12 19:59:43 +0200

    Loader

--- src/components/StylePreview.tsx
+++ src/components/StylePreview.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ScaleLoader from "react-spinners/ScaleLoader";
+import BounceLoader from "react-spinners/BounceLoader";
 
 import { theme } from "~/styles/tailwind";
 
@@ -12,7 +12,8 @@ export const StylePreview: React.FC<Props> = ({ src }) => {
     <div className="flex relative justify-center items-center w-32 h-32 mt-1 border border-gray-700">
       {!src && <span className="text-xs text-gray-500">Preview unavailable</span>}
 
-      <ScaleLoader color={theme.colors.green[500]} height={12} width={2} />
+      <BounceLoader color={theme.colors.green[500]} size={20} />
+
       {src && <img className="absolute" src={src} />}
     </div>
   );
