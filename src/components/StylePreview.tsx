52d10891074265bfa0effda37ebe7d8808496032
Bastien Formery <bastien.formery@gmail.com>
2020-10-19 22:28:01 +0200

    Fix CSS

--- src/components/StylePreview.tsx
+++ src/components/StylePreview.tsx
@@ -13,8 +13,8 @@ export const StylePreview: React.FC<Props> = ({ src, hash }) => {
   return (
     <div className="flex relative justify-center items-center w-full h-full mt-1 border overflow-hidden">
       {hash && (
-        <div className="absolute">
-          <Blurhash hash={hash} height={120} punch={1} resolutionX={32} resolutionY={32} width={215} />
+        <div className="absolute w-full h-full">
+          <Blurhash hash={hash} height="100%" punch={1} resolutionX={32} resolutionY={32} width="100%" />
         </div>
       )}
 
