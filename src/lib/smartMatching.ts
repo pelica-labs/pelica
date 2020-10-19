cabb163cf4f328c1a44b0291d7e602651a9fc7c1
Benjamin TD <benjamin.tdm@gmail.com>
2020-10-19 11:02:06 +0200

    prepare for directions matching

--- src/lib/smartMatching.ts
+++ src/lib/smartMatching.ts
@@ -1,9 +1,11 @@
 import { MapboxProfile } from "@mapbox/mapbox-sdk/lib/classes/mapi-request";
 import { Tracepoint } from "@mapbox/mapbox-sdk/services/map-matching";
+import polyline from "@mapbox/polyline";
 import { Position } from "@turf/turf";
+import CheapRuler from "cheap-ruler";
 import { chunk } from "lodash";
 
-import { mapboxMapMatching } from "~/lib/mapbox";
+import { mapboxDirections, mapboxMapMatching } from "~/lib/mapbox";
 
 export type SmartMatching = {
   enabled: boolean;
@@ -12,7 +14,7 @@ export type SmartMatching = {
 
 export type SmartMatchingProfile = MapboxProfile;
 
-export const smartMatch = async (points: Position[], profile: SmartMatchingProfile): Promise<Position[]> => {
+export const mapMatch = async (points: Position[], profile: SmartMatchingProfile): Promise<Position[]> => {
   const chunks = await Promise.all(
     chunk(points, 100).map(async (points) => {
       if (points.length < 2) {
@@ -34,7 +36,7 @@ export const smartMatch = async (points: Position[], profile: SmartMatchingProfi
         .send();
 
       if (!res.body.tracepoints) {
-        console.info("Smart matching did return any tracepoints", res.body);
+        console.info("Map matching did return any tracepoints", res.body);
 
         return points;
       }
@@ -50,4 +52,42 @@ export const smartMatch = async (points: Position[], profile: SmartMatchingProfi
   );
 
   return chunks.flat();
+};
+
+export const directionsMatch = async (points: Position[], profile: SmartMatchingProfile): Promise<Position[]> => {
+  const chunks = await Promise.all(
+    chunk(points, 25).map(async (points) => {
+      if (points.length < 2) {
+        return points;
+      }
+
+      const res = await mapboxDirections
+        .getDirections({
+          profile,
+          waypoints: points.map((point) => {
+            return {
+              coordinates: point,
+            };
+          }),
+        })
+        .send();
+
+      if (!res.body.routes?.length || !res.body.routes[0].geometry) {
+        console.info("Directions matching did return any routes", res.body);
+
+        return points;
+      }
+
+      return polyline.toGeoJSON(res.body.routes[0].geometry as string).coordinates;
+    })
+  );
+
+  return chunks.flat();
+};
+
+export const smartMatch = async (points: Position[], profile: SmartMatchingProfile): Promise<Position[]> => {
+  // @todo depending on distance between points, use mapMatch or directions match.
+  // if (points.length === 0) return [];
+  // const ruler = new CheapRuler(points[0][1], "meters");
+  return mapMatch(points, profile);
 };
