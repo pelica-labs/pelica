cb936678433c09f648be73f0fd6b3e331b9bc5c7
Bastien Formery <bastien.formery@gmail.com>
2020-10-26 14:44:03 +0100

    Case sensitive

---
+++ src/components/sidebar/DrawSidebar.tsx
@@ -0,0 +1,135 @@
+import React, { useRef } from "react";
+
+import { Button } from "~/components/Button";
+import { ColorPicker } from "~/components/ColorPicker";
+import { Distance } from "~/components/Distance";
+import { OutlineSelector } from "~/components/OutlineSelector";
+import { SidebarHeader, SidebarHeading, SidebarSection } from "~/components/sidebar/Sidebar";
+import { SmartMatchingSelector } from "~/components/SmartMatchingSelector";
+import { WidthSlider } from "~/components/WidthSlider";
+import { app, useStore } from "~/core/app";
+import { computeDistance, Route } from "~/core/routes";
+import { getSelectedEntity } from "~/core/selectors";
+
+export const DrawSidebar: React.FC = () => {
+  const fileInput = useRef<HTMLInputElement>(null);
+  const color = useStore((store) => store.routes.style.color);
+  const width = useStore((store) => store.routes.style.width);
+  const outline = useStore((store) => store.routes.style.outline);
+  const smartMatching = useStore((store) => store.routes.smartMatching);
+  const route = useStore((store) => getSelectedEntity(store) as Route);
+  const screenDimensions = useStore((store) => store.platform.screen.dimensions);
+
+  return (
+    <>
+      <SidebarSection>
+        <SidebarHeader className="px-1">
+          <SidebarHeading>Color</SidebarHeading>
+          <div className="ml-2 w-3 h-3 rounded-full border border-gray-200" style={{ backgroundColor: color }} />
+        </SidebarHeader>
+        <div className="mt-4">
+          <ColorPicker
+            value={color}
+            onChange={(color) => {
+              app.routes.setStyle({ color });
+            }}
+            onChangeComplete={(color) => {
+              app.routes.setStyle({ color });
+            }}
+          />
+        </div>
+      </SidebarSection>
+
+      <SidebarSection>
+        <SidebarHeader>
+          <SidebarHeading>Width</SidebarHeading>
+          <div className="ml-2 flex justify-center items-center w-3 h-3 rounded-full">
+            <div className="rounded-full" style={{ width: width, height: width, backgroundColor: color }} />
+          </div>
+        </SidebarHeader>
+        <div className="mt-5 md:mt-4 px-1 md:w-full flex-1 flex justify-center mb-5 md:mb-0">
+          <WidthSlider
+            color={color}
+            max={12}
+            min={1}
+            value={width}
+            onChange={(width) => {
+              app.routes.setStyle({ width });
+            }}
+            onChangeComplete={(width) => {
+              app.routes.setStyle({ width });
+            }}
+          />
+        </div>
+      </SidebarSection>
+
+      <SidebarSection>
+        <SidebarHeader>
+          <SidebarHeading>Outline</SidebarHeading>
+        </SidebarHeader>
+        <div className="mt-4 w-40" style={{ marginLeft: screenDimensions.md ? -4 : 0 }}>
+          <OutlineSelector
+            value={outline}
+            onChange={(outline) => {
+              app.routes.setStyle({ outline });
+            }}
+          />
+        </div>
+      </SidebarSection>
+
+      <SidebarSection>
+        <SidebarHeader>
+          <SidebarHeading>Routes</SidebarHeading>
+        </SidebarHeader>
+
+        <div className="mt-5 md:mt-4 w-40 md:w-full">
+          <SmartMatchingSelector
+            value={smartMatching}
+            onChange={(smartMatching) => {
+              app.routes.setSmartMatching(smartMatching);
+            }}
+          />
+        </div>
+      </SidebarSection>
+
+      <SidebarSection>
+        <SidebarHeader className="flex flex-col items-start space-y-4 md:space-y-0 md:flex-row md:items-center md:justify-between md:w-full">
+          <SidebarHeading>Import</SidebarHeading>
+          <Button
+            className="bg-gray-300 text-gray-800"
+            onClick={() => {
+              fileInput.current?.click();
+            }}
+          >
+            <input
+              ref={fileInput}
+              className="hidden"
+              type="file"
+              onChange={(event) => {
+                if (event.target.files?.length) {
+                  app.imports.importGpx(event.target.files[0]);
+                }
+              }}
+            />
+            <span className="text-xs">GPX</span>
+          </Button>
+        </SidebarHeader>
+      </SidebarSection>
+
+      {route && (
+        <SidebarSection className="md:mt-auto">
+          <SidebarHeader>
+            <SidebarHeading>Inspect</SidebarHeading>
+          </SidebarHeader>
+
+          <div className="mt-5 md:mt-4">
+            <div className="flex items-center text-xs">
+              <span className="mr-4">Distance</span>
+              <Distance value={computeDistance(route)} />
+            </div>
+          </div>
+        </SidebarSection>
+      )}
+    </>
+  );
+};
---
+++ src/components/sidebar/ExportSidebar.tsx
@@ -0,0 +1,227 @@
+import { format } from "date-fns";
+import React, { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import BounceLoader from "react-spinners/BounceLoader";
+
+import { AspectRatioSelector } from "~/components/AspectRatioSelector";
+import { Button } from "~/components/Button";
+import { CopyIcon } from "~/components/Icon";
+import { SidebarHeader, SidebarHeading, SidebarSection } from "~/components/sidebar/Sidebar";
+import { app, useStore } from "~/core/app";
+import { getMapTitle } from "~/core/selectors";
+import { useBrowserFeatures } from "~/hooks/useBrowserFeatures";
+import { aspectRatios } from "~/lib/aspectRatio";
+import { dataUrlToBlob } from "~/lib/fileConversion";
+import { theme } from "~/styles/tailwind";
+
+export const ExportSidebar: React.FC = () => {
+  const { t } = useTranslation();
+  const imageData = useStore((store) => store.exports.imageData);
+  const aspectRatio = useStore((store) => store.editor.aspectRatio);
+  const screenDimensions = useStore((store) => store.platform.screen.dimensions);
+  const { ratio } = aspectRatios[aspectRatio];
+
+  const [imageBlob, setImageBlob] = useState<Blob | null>(null);
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+
+  const { shareFeature } = useBrowserFeatures();
+
+  const onDownload = () => {
+    if (!imageData) {
+      return;
+    }
+    const a = document.createElement("a");
+    a.href = imageData;
+    a.download = [t("pelica"), getMapTitle(), format(Date.now(), "yyyy-MM-dd HH-mm-ss")]
+      .filter((text) => !!text)
+      .join(" · ");
+    a.click();
+  };
+
+  const onShare = () => {
+    if (!imageUrl) {
+      return;
+    }
+
+    navigator.share({
+      title: [t("pelica"), getMapTitle()].filter((text) => !!text).join(" · "),
+      url: imageUrl,
+    });
+  };
+
+  const onCopy = async () => {
+    if (!imageUrl) {
+      return;
+    }
+
+    await navigator.clipboard.writeText(imageUrl);
+
+    app.alerts.trigger({
+      color: "green",
+      icon: CopyIcon,
+      message: "Map URL has been copied to clipboard.",
+    });
+  };
+
+  useEffect(() => {
+    app.exports.prepareCanvas();
+  }, []);
+
+  useEffect(() => {
+    if (!imageData) {
+      return;
+    }
+
+    setImageBlob(dataUrlToBlob(imageData));
+  }, [imageData]);
+
+  useEffect(() => {
+    if (!imageBlob) {
+      return;
+    }
+
+    const timeout = setTimeout(() => {
+      const data = new FormData();
+
+      data.append("image", imageBlob);
+
+      const mapTitle = getMapTitle();
+      if (mapTitle) {
+        data.append("name", mapTitle);
+      }
+
+      fetch("/api/upload-map", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+        },
+        body: data,
+      })
+        .then((res) => res.json())
+        .then((json) => {
+          setImageUrl(json.url);
+        })
+        .catch((error) => {
+          // @todo: handle error
+          console.error(error);
+        });
+    }, 200);
+
+    return () => clearTimeout(timeout);
+  }, [imageBlob]);
+
+  return (
+    <>
+      <div className="flex md:flex-col md:divide-y md:divide-x-0 divide-x md:h-full">
+        <SidebarSection className="flex flex-col space-y-1 md:space-y-2 w-48 md:w-auto">
+          <AspectRatioSelector
+            value={aspectRatio}
+            onChange={(aspectRatio) => {
+              app.editor.setAspectRatio(aspectRatio);
+            }}
+          />
+        </SidebarSection>
+
+        <SidebarSection className="flex flex-col space-y-3 w-40 md:w-auto">
+          {!screenDimensions.md && (
+            <SidebarHeader>
+              <SidebarHeading>Export</SidebarHeading>
+            </SidebarHeader>
+          )}
+
+          <Button
+            className="bg-orange-100 text-gray-800 border border-orange-200 hover:border-orange-100 text-xs uppercase py-2 justify-center w-full"
+            disabled={!imageData}
+            onClick={() => {
+              onDownload();
+            }}
+          >
+            Download
+            {!imageData && (
+              <div className="ml-4">
+                <BounceLoader color={theme.colors.orange[500]} size={10} />
+              </div>
+            )}
+          </Button>
+
+          {shareFeature && (
+            <Button
+              className="bg-orange-100 text-gray-800 border border-orange-200 hover:border-orange-100 text-xs uppercase py-2 justify-center w-full"
+              disabled={!imageUrl}
+              onClick={() => {
+                onShare();
+              }}
+            >
+              Share
+              {!imageUrl && (
+                <div className="ml-4">
+                  <BounceLoader color={theme.colors.orange[500]} size={10} />
+                </div>
+              )}
+            </Button>
+          )}
+          {!shareFeature && (
+            <Button
+              className="bg-orange-100 text-gray-800 border border-orange-200 hover:border-orange-100 text-xs uppercase py-2 justify-center w-full"
+              disabled={!imageUrl}
+              onClick={() => {
+                onCopy();
+              }}
+            >
+              Copy URL
+              {!imageUrl && (
+                <div className="ml-4">
+                  <BounceLoader color={theme.colors.orange[500]} size={10} />
+                </div>
+              )}
+            </Button>
+          )}
+        </SidebarSection>
+
+        <SidebarSection className="flex flex-col space-y-3 w-64 md:w-auto">
+          <SidebarHeader>
+            <SidebarHeading>Output format</SidebarHeading>
+          </SidebarHeader>
+          <div className="flex-col md:space-y-1">
+            <div className="text-xs flex justify-between">
+              <span className="flex-1 mr-4">Format</span>
+              <span>JPEG</span>
+            </div>
+            {ratio && (
+              <>
+                <div className="text-xs flex justify-between">
+                  <span className="flex-1 mr-4">Resolution</span>
+                  <span>
+                    {ratio[0].toFixed(0)} <span className="text-gray-500">×</span> {ratio[1].toFixed(0)}{" "}
+                    <span className="text-gray-500">px</span>
+                  </span>
+                </div>
+              </>
+            )}
+          </div>
+        </SidebarSection>
+
+        <div className="md:mt-auto px-3 md:pt-3 md:pb-2 w-64 md:w-auto">
+          <SidebarHeader>
+            <SidebarHeading>Copyright</SidebarHeading>
+          </SidebarHeader>
+
+          <div className="flex justify-between items-center mt-2 overflow-x-hidden">
+            <span className="text-2xs leading-tight">
+              Prints use map data and styles from Mapbox and OpenStreetMap and must be attributed accordingly. To learn
+              more, see{" "}
+              <a className="underline" href="https://docs.mapbox.com/help/how-mapbox-works/attribution/">
+                Mapbox
+              </a>{" "}
+              and{" "}
+              <a className="underline" href="http://www.openstreetmap.org/copyright">
+                OSM
+              </a>{" "}
+              attribution requirements.
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
