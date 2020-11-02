e67dd8f3cb65f5e68a10984e6660eee11b8a3ab2
Bastien Formery <bastien.formery@gmail.com>
2020-11-02 19:13:06 +0100

    Bullshit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { useTranslation } from "react-i18next";
 
 import { Container } from "~/components/Container";
 import { Footer } from "~/components/Footer";
+import { DownloadIcon, ExportIcon, PinIcon, StyleIcon } from "~/components/Icon";
 import { Navbar } from "~/components/Navbar";
 
 const Home: NextPage = () => {
@@ -82,9 +83,90 @@ const Home: NextPage = () => {
             </button>
           </div>
         </div>
+
+        <Features />
       </Container>
 
       <Footer />
+    </div>
+  );
+};
+
+const Features: React.FC = () => {
+  return (
+    <div className="mt-20 py-12">
+      <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="lg:text-center">
+          <h3 className="mt-2 text-3xl leading-8 font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 font-raleway">
+            The easiest way to design maps
+          </h3>
+        </div>
+
+        <div className="mt-16">
+          <ul className="md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+            <li>
+              <div className="flex">
+                <div className="flex-shrink-0">
+                  <div className="flex items-center justify-center h-12 w-12 rounded-md bg-orange-400 text-white rounded">
+                    <StyleIcon className="w-6 h-6" />
+                  </div>
+                </div>
+                <div className="ml-4">
+                  <h4 className="text-lg leading-6 font-medium text-gray-900">Beautiful curated styles</h4>
+                  <p className="mt-2 text-base leading-6 text-gray-500">
+                    Select from a list of pre-designed styles. Your maps will look good no matter what.
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li className="mt-10 md:mt-0">
+              <div className="flex">
+                <div className="flex-shrink-0">
+                  <div className="flex items-center justify-center h-12 w-12 rounded-md bg-orange-400 text-white rounded">
+                    <PinIcon className="w-6 h-6" />
+                  </div>
+                </div>
+                <div className="ml-4">
+                  <h4 className="text-lg leading-6 font-medium text-gray-900">Simple yet powerful drawing tools</h4>
+                  <p className="mt-2 text-base leading-6 text-gray-500">
+                    Draw routes, add pins, write texts. That's about it.
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li className="mt-10 md:mt-0">
+              <div className="flex">
+                <div className="flex-shrink-0">
+                  <div className="flex items-center justify-center h-12 w-12 rounded-md bg-orange-400 text-white rounded">
+                    <ExportIcon className="w-6 h-6" />
+                  </div>
+                </div>
+                <div className="ml-4">
+                  <h4 className="text-lg leading-6 font-medium text-gray-900">Built for sharing</h4>
+                  <p className="mt-2 text-base leading-6 text-gray-500">
+                    Share interactive maps or just raw images right from the app.
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li className="mt-10 md:mt-0">
+              <div className="flex">
+                <div className="flex-shrink-0">
+                  <div className="flex items-center justify-center h-12 w-12 rounded-md bg-orange-400 text-white rounded">
+                    <DownloadIcon className="w-6 h-6" />
+                  </div>
+                </div>
+                <div className="ml-4">
+                  <h4 className="text-lg leading-6 font-medium text-gray-900">High resolution exports</h4>
+                  <p className="mt-2 text-base leading-6 text-gray-500">
+                    Generate high res images, suited for print / professional work. Don't compromise on quality.
+                  </p>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
