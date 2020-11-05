99958f49a7f86e7ae6ae48541600e7e005951714
Bastien Formery <bastien.formery@gmail.com>
2020-11-05 13:44:38 +0100

    Display warning when inserting text with emoji

--- src/components/LabelTextareaField.tsx
+++ src/components/LabelTextareaField.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import TextareaAutosize from "react-textarea-autosize";
 
+import { PinIcon, WarningIcon } from "~/components/Icon";
+
 type Props = {
   value: string;
 
@@ -10,17 +12,33 @@ type Props = {
 
 export const LabelTextareaField: React.FC<Props> = ({ value, onChange, onChangeComplete }) => {
   return (
-    <TextareaAutosize
-      className="border rounded p-1 w-full text-xs"
-      maxRows={8}
-      minRows={3}
-      value={value}
-      onBlur={(event) => {
-        onChangeComplete(event.target.value);
-      }}
-      onChange={(event) => {
-        onChange(event.target.value);
-      }}
-    />
+    <div>
+      <TextareaAutosize
+        className="border rounded p-1 w-full text-xs"
+        maxRows={8}
+        minRows={3}
+        value={value}
+        onBlur={(event) => {
+          onChangeComplete(event.target.value);
+        }}
+        onChange={(event) => {
+          onChange(event.target.value);
+        }}
+      />
+
+      {containsUnsupportedCharacter(value) && (
+        <p className="mx-1 text-gray-600 flex space-x-1">
+          <span className="text-2xs">
+            <WarningIcon className="w-3 h-3 inline mr-1" />
+            Special characters such as emoji or unicode aren't supported yet. You can use{" "}
+            <PinIcon className="w-3 h-3 inline" /> pins for emojis.
+          </span>
+        </p>
+      )}
+    </div>
   );
+};
+
+const containsUnsupportedCharacter = (string: string) => {
+  return /[\uD800-\uDFFF]/.test(string);
 };
