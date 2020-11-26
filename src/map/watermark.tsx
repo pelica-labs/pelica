33a8d76c994f859a6ac3c554bdf42fc17c7828df
Benjamin TD <benjamin.tdm@gmail.com>
2020-11-26 15:55:23 +0100

    fix watermark on safari

--- src/map/watermark.tsx
+++ src/map/watermark.tsx
@@ -8,7 +8,7 @@ export const applyWatermark = (): void => {
 
   const watermark = ReactDOMServer.renderToString(
     <div className="absolute bottom-0 left-0 flex mb-1 ml-2" id="watermark">
-      <img alt="Watermark" className="w-24" src="/images/watermark.png" />
+      <img alt="Watermark" className="w-24 object-contain object-bottom" src="/images/watermark.png" />
     </div>
   );
   const container = document.createElement("div");
