0aeea43650eef02b9da713d478277b724a2693e6
Bastien Formery <bastien.formery@gmail.com>
2020-12-11 13:36:27 +0100

    Terrain

---
+++ src/map/terrain.ts
@@ -0,0 +1,24 @@
+import { getMap } from "~/core/selectors";
+
+export const applyTerrain = (): void => {
+  const map = getMap();
+
+  map.addSource("mapbox-dem", {
+    type: "raster-dem",
+    url: "mapbox://mapbox.mapbox-terrain-dem-v1",
+    tileSize: 128,
+    maxzoom: 14,
+  });
+
+  map.setTerrain({ source: "mapbox-dem", exaggeration: 1.5 });
+
+  map.addLayer({
+    id: "sky",
+    type: "sky",
+    paint: {
+      "sky-type": "atmosphere",
+      "sky-atmosphere-sun": [0.0, 0.0],
+      "sky-atmosphere-sun-intensity": 15,
+    },
+  });
+};
