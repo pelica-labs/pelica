530c91145cb60cdc8fbdf8515938f509caa7e7c5
Bastien Formery <bastien.formery@gmail.com>
2020-12-22 11:32:38 +0100

    UI

--- src/components/map/MapViewer.tsx
+++ src/components/map/MapViewer.tsx
@@ -1,6 +1,7 @@
 import { useSession } from "next-auth/client";
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 
 import { GeolocationButton } from "~/components/map/GeolocationButton";
 import { Map } from "~/components/map/Map";
@@ -8,7 +9,7 @@ import { PlaceAutocomplete } from "~/components/map/PlaceAutocomplete";
 import { ResetOrientationButton } from "~/components/map/ResetOrientationButton";
 import { Button } from "~/components/ui/Button";
 import { Heading } from "~/components/ui/Heading";
-import { EditIcon } from "~/components/ui/Icon";
+import { CopyIcon, EditIcon } from "~/components/ui/Icon";
 import { app, useStore } from "~/core/app";
 import { MapModel } from "~/core/db";
 
@@ -17,10 +18,32 @@ type Props = {
 };
 
 export const MapViewer: React.FC<Props> = ({ map }) => {
+  const router = useRouter();
   const place = useStore((store) => store.map.place);
   const currentCoordinates = useStore((store) => store.map.coordinates);
   const currentZoom = useStore((store) => store.map.zoom);
   const [session] = useSession();
+  const [showCopyMessage, setShowCopyMessage] = useState(true);
+  const mapBelongsToCurrentUser = session?.user.id === map.userId;
+
+  const onCloneMap = async () => {
+    setShowCopyMessage(false);
+
+    const res = await fetch("/api/clone-map", {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: map.id,
+      }),
+    });
+
+    const json = await res.json();
+
+    router.push(`/app/${json.id}`);
+  };
 
   return (
     <div className="flex flex-col md:flex-row h-full justify-between bg-gray-200">
@@ -43,7 +66,7 @@ export const MapViewer: React.FC<Props> = ({ map }) => {
         <ResetOrientationButton />
       </div>
 
-      {session?.user.id === map.userId && (
+      {mapBelongsToCurrentUser && (
         <div className="absolute bottom-0 left-0 right-0">
           <div className="w-40 mx-auto bg-white rounded-lg mb-2 shadow p-2 space-y-4 border-l-4 border-r-4 border-orange-400 flex flex-col items-center">
             <Heading>Preview mode</Heading>
@@ -54,6 +77,31 @@ export const MapViewer: React.FC<Props> = ({ map }) => {
                 <span className="ml-2 text-xs">Back to edition</span>
               </Button>
             </Link>
+          </div>
+        </div>
+      )}
+
+      {showCopyMessage && !mapBelongsToCurrentUser && (
+        <div className="absolute bottom-0 left-0 right-0">
+          <div className="mx-auto w-64 bg-white rounded-lg mb-2 shadow p-2 space-x-2 border-l-4 border-r-4 border-orange-400 flex justify-between items-center">
+            <Button
+              className="relative bg-white border border-gray-200 text-gray-800 shadow flex justify-center items-center transition-all duration-100 ease-in-out cursor-pointer rounded-full hover:border-orange-300"
+              onClick={() => {
+                onCloneMap();
+              }}
+            >
+              <CopyIcon className="w-4 h-4" />
+              <span className="ml-2 text-xs">Copy to my account</span>
+            </Button>
+
+            <Button
+              className="relative bg-white border border-gray-200 text-gray-800 shadow flex justify-center items-center transition-all duration-100 ease-in-out cursor-pointer rounded-full hover:border-orange-300"
+              onClick={() => {
+                setShowCopyMessage(false);
+              }}
+            >
+              <span className="text-xs">Hide</span>
+            </Button>
           </div>
         </div>
       )}
