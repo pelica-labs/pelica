053de9b5fe15ef78d7e36d1c55ef0a2202dd93f7
Bastien Formery <bastien.formery@gmail.com>
2020-12-23 14:03:34 +0100

    Fix useAsyncStorage React warning

--- src/hooks/useAsyncStorage.ts
+++ src/hooks/useAsyncStorage.ts
@@ -1,5 +1,5 @@
 import localforage from "localforage";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const asyncStorage = localforage.createInstance({
   name: "pelica",
@@ -8,17 +8,24 @@ export const asyncStorage = localforage.createInstance({
 });
 
 export const useAsyncStorage = <T>(key: string, initialValue: T): [T, (value: T) => Promise<void>] => {
+  const isMounted = useRef(true);
   const [storedValue, setStoredValue] = useState(initialValue);
 
   useEffect(() => {
     getItem();
+
+    return () => {
+      isMounted.current = false;
+    };
   }, []);
 
   const getItem = async () => {
     try {
       const value = await asyncStorage.getItem<T>(key);
 
-      setStoredValue(value ?? initialValue);
+      if (isMounted.current) {
+        setStoredValue(value ?? initialValue);
+      }
     } catch (error) {
       console.error(error);
     }
@@ -26,7 +33,10 @@ export const useAsyncStorage = <T>(key: string, initialValue: T): [T, (value: T)
 
   const setValue = async (value: T) => {
     const valueToStore = value instanceof Function ? value(storedValue) : value;
-    setStoredValue(valueToStore);
+
+    if (isMounted.current) {
+      setStoredValue(valueToStore);
+    }
 
     try {
       await asyncStorage.setItem(key, value);
